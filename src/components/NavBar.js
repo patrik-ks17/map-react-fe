50704e43c39fb56cb0eff72b19ea9545fa04aff6
KasaPatrik <kasaip02@gmail.com>
2023-02-03 13:58:56 +0100

    main

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,12 +12,12 @@ function NavBar() {
   return (
 	 <div className='navbar'>
 		<ul>
-			<li><img data-name={"setting"} src='icon/navbar/setting.png' onClick={(e) => handleClick(e)}></img></li>
-			<li><img data-name={"user"} src='icon/navbar/user.png' onClick={(e) => handleClick(e)}></img></li>
-			<li><img data-name={"chat"} src='icon/navbar/fire.png' onClick={(e) => handleClick(e)}></img></li>
-			<li><img data-name={"sports"} src='icon/navbar/sports.png' onClick={(e) => handleClick(e)}></img></li>
-			<li><img data-name={"map"} src='icon/navbar/location.png' onClick={(e) => handleClick(e)}></img></li>
-			<li><img data-name={"info"} src='icon/navbar/information.png' onClick={(e) => handleClick(e)}></img></li>
+			<li><img data-name={"setting"} src='icon/navbar/setting.png' alt="setting" onClick={(e) => handleClick(e)}></img></li>
+			<li><img data-name={"login"} src='icon/navbar/user.png' alt="user" onClick={(e) => handleClick(e)}></img></li>
+			<li><img data-name={"chat"} src='icon/navbar/firechat.png' alt="chat" onClick={(e) => handleClick(e)}></img></li>
+			<li><img data-name={"sports"} src='icon/navbar/sports.png' alt="sports" onClick={(e) => handleClick(e)}></img></li>
+			<li><img data-name={"map"} src='icon/navbar/map.png' alt="map" onClick={(e) => handleClick(e)}></img></li>
+			<li><img data-name={"info"} src='icon/navbar/info.png' alt="info" onClick={(e) => handleClick(e)}></img></li>
 		</ul>
 	 </div>
   )
